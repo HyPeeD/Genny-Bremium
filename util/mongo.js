8c54e29e21b11d8b3eeb01e67218836e3c7ebfd2
CutiePie <51923365+xCutieePiee@users.noreply.github.com>
2021-10-19 16:25:28 +0100

    Update mongo.js

--- util/mongo.js
+++ util/mongo.js
@@ -1,12 +1,7 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
 module.exports = async (database) => {
-	try {
-		await mongoose.connect(database, { useNewUrlParser: true, useUnifiedTopology: true })
-		return mongoose
-	} catch (e) {
-		await mongoose.connection.close()
-		await mongoose.connect(database, { useNewUrlParser: true, useUnifiedTopology: true })
-		return mongoose
-	}
+	if (mongoose.connection) await mongoose.connection.close()
+	await mongoose.connect(database, { useNewUrlParser: true, useUnifiedTopology: true })
+	return mongoose
 }
