3d9448019c20d6aaf9fc42579f8fce98647b2096
CutiePie <51923365+xCutieePiee@users.noreply.github.com>
2021-10-28 20:33:38 +0100

    Update music-modify.js

--- util/music-modify.js
+++ util/music-modify.js
@@ -1,13 +1,10 @@
 module.exports = {
   canModifyQueue(member, send) {
-    const { channel } = member.voice;
-    const botChannel = member.guild.me.voice.channel;
+    const { channel } = member.voice
+    const botChannel = member.guild.me.voice.channel
 
-    if (channel !== botChannel) {
-      send.send(':no_entry_sign: You must be listening in **'+member.guild.me.voice.channel.name+"** to use that!").catch(console.error);
-      return false;
-    }
+    if (channel !== botChannel) return false
 
-    return true;
+    return true
   }
-};
+}
